3ca626bafee1d7740497252e9da0a3c1f38fd2b4
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-04 10:16:16 +0330

    Add files via upload

---
+++ WindowsFormsApp59/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp59
+{
+    public class student
+    {
+        private string cd;
+        public string DateofBorn
+        {
+            get { return cd; }
+            set
+            {
+                System.Globalization.PersianCalendar P = new System.Globalization.PersianCalendar();
+
+                cd = P.GetYear(DateTime.Parse(value)).ToString() + "/" + P.GetMonth(DateTime.Parse(value)).ToString() + P.GetDayOfMonth(DateTime.Parse(value)).ToString();
+
+
+            }
+
+        }
+
+        }
+
+    }
+
+    
+
+
+
